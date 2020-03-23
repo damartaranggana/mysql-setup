90b97a89fdb48ee30ef46d09704a8a1f14e37c6d
damartaranggana <damartaranggana158@gmail.com>
2020-03-24 00:53:10 +0700

    Delete mysql.sh

--- mysql.sh
+++
@@ -1,8 +0,0 @@
-apt update
-apt install mysql-server
-mysql_secure_installation
-nano /etc/mysql/mysql.conf.d/mysqld.cnf
-mysql
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'*' IDENTIFIED BY 'Kocak//2000' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-exit
